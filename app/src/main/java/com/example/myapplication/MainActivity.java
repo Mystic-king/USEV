fc1d4e2648ba0220caa8bccf07fd905afeaac158
shreyasmavale <Mavaleshreyas12@gmail.com>
2022-02-17 16:28:01 +0530

    FRONTEND

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity<textview> extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+    public void login(View view ){
+
+    }
+
+
+}
